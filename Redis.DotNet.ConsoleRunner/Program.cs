476c079c6abda83077d76c3252a9f747cce412a4
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2021-11-23 22:59:54 +0300

    StackExchange.Redis

---
+++ Redis.DotNet.ConsoleRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Redis.DotNet.ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () => await new RedisProcessor().Run()).Wait();
+            Console.ReadKey();
+        }
+    }
+}
--- Redis.DotNet/RedisProcessor.cs
+++ Redis.DotNet/RedisProcessor.cs
@@ -16,6 +16,22 @@ namespace Redis.DotNet
             var db = redis.GetDatabase();
             var pong = await db.PingAsync();
             Console.WriteLine(pong);
+
+            var result = await db.StringSetAsync("setKey", "setValue");
+            Console.WriteLine(result);
+
+            var redisValue = await db.StringGetAsync("setKey");
+            var redisValueObject = redisValue.Box();
+
+            Console.WriteLine(redisValueObject);
+
+            ISubscriber sub = redis.GetSubscriber();
+            sub.Subscribe("messages").OnMessage(async channelMessage => {
+                await Task.Delay(1000);
+                Console.WriteLine(channelMessage.Message);
+            });
+
+            await sub.PublishAsync("messages", "hello");
         }
     }
 }
