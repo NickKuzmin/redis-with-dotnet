9831296b5f8f759b457a5ddf927529cbc4d761dc
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2021-11-23 14:26:34 +0300

    Initial commit

---
+++ Redis.DotNet/RedisProcessor.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+namespace Redis.DotNet
+{
+    public class RedisProcessor
+    {
+        public async Task Run()
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(new ConfigurationOptions
+            {
+                EndPoints = { "localhost:6379" }
+            });
+
+            var db = redis.GetDatabase();
+            var pong = await db.PingAsync();
+            Console.WriteLine(pong);
+        }
+    }
+}
